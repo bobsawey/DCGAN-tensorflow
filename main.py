46b4fb43acb2d025a0d94bc417b07b105b548f0d
Bobby Jo <bobsawey@gmail.com>
2018-07-28 22:14:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ FLAGS = flags.FLAGS
 ####Delete all flags before declare#####
 
 def del_all_flags(FLAGS):
-    flags_dict = FLAGS._flags()    
-    keys_list = [keys for keys in flags_dict]    
-    for keys in keys_list:    
-    FLAGS.__delattr__(keys)
+  flags_dict = FLAGS._flags()    
+  keys_list = [keys for keys in flags_dict]    
+  for keys in keys_list:    
+  FLAGS.__delattr__(keys)
 
 del_all_flags(tf.flags.FLAGS)
 
